eb0638f37321d4ac3aebe83358952911faf689cb
Caylem <caylemh@gmail.com>
2022-06-09 14:29:05 +0200

    Create Restaurant Container

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <TouchableOpacity style={styles.container}>
+      {/* Restuarant Item */}
+      <View style={styles.restaurantContainer}>
+        <View style={styles.imageContainer}>
+          <Image source={{
+            uri:'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/uber-eats/restaurant1.jpeg'
+            }} 
+            style={styles.image}
+          />
+        </View>
+        <View style={styles.restaurantInfo}>
+          <View style={styles.desc}>
+            <Text style={styles.title}>El Cabo Coffe Bar Tres De Mayo</Text>
+            <Text style={styles.subTitle}>$1.40 25-35 min</Text>
+          </View>
+          <View style={styles.rating}>
+              <Text>4.0</Text>
+          </View>
+        </View>
+      </View>
+
+
       <StatusBar style="auto" />
-    </View>
+    </TouchableOpacity>
   );
 }
 
@@ -16,5 +36,40 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 10
+  },
+  restaurantContainer:{
+    width: '100%',
+  },
+  restaurantInfo:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  
+  },
+  imageContainer:{
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.5,
+  },
+  image:{
+    width: '100%',
+    aspectRatio: 5/3,
+    marginVertical: 10,
+    borderRadius: 10,
+  },
+  title:{
+    fontSize: 18,
+    fontWeight: '700'
+  },
+  subTitle:{
+    color: 'grey',
+    marginTop: 5
+  },
+  rating:{
+    backgroundColor: '#eee',
+    padding: 8,
+    height: 35,
+    justifyContent: 'center',
+    borderRadius: '20%'
   },
 });
